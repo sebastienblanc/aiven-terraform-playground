ee92c361cacbba4e4f30ebf876df7a007068377e
Sébastien Blanc <scm.blanc@gmail.com>
2022-11-23 12:08:22 +0100

    add a topic config

--- kafka.tf
+++ kafka.tf
@@ -25,3 +25,11 @@ resource "aiven_kafka_topic" "topic-customer-activity" {
   partitions   = 3
   replication  = 2
 }
+
+resource "aiven_kafka_topic" "topic-customer-payments" {
+  project      = "dev-sandbox"
+  service_name = aiven_kafka.sblanc-kafka-terraform-test.service_name
+  topic_name   = "customer-activity-payments"
+  partitions   = 3
+  replication  = 2
+}
