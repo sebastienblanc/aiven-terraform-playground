7c283fb8c6d90eb29a29d90155f845093d33fceb
Sébastien Blanc <scm.blanc@gmail.com>
2023-01-12 08:37:18 +0100

    remove a topic

--- kafka.tf
+++ kafka.tf
@@ -26,10 +26,3 @@ resource "aiven_kafka_topic" "topic-customer-activity" {
   replication  = 2
 }
 
-resource "aiven_kafka_topic" "topic-other-activity" {
-  project      = "dev-sandbox"
-  service_name = aiven_kafka.sblanc-kafka-terraform-test.service_name
-  topic_name   = "other-activity"
-  partitions   = 3
-  replication  = 2
-}
