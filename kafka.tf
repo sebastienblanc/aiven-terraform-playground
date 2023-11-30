dfe3f24f2a62085c4b5bd814e992c48e6ebb09a9
Sebastien Blanc <scm.blanc@gmail.com>
2023-11-30 11:37:39 +0100

    Update kafka.tf

--- kafka.tf
+++ kafka.tf
@@ -33,14 +33,6 @@ resource "aiven_kafka_topic" "topic-live-coding" {
   replication  = 2
 }
 
-resource "aiven_kafka_topic" "topic-live-now-itwillwork" {
-  project      = "devrel-sebastien"
-  service_name = aiven_kafka.sblanc-kafka-terraform-gh.service_name
-  topic_name   = "live-for-decathlon"
-  partitions   = 6
-  replication  = 2
-}
-
 
 
 
